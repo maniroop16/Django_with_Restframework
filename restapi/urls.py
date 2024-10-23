2f4e0fde268b4fd4126877dcb919e9ea2f30a10f
LTIsaimaniroop <saimaniroop.10687590@ltimindtree.com>
2024-10-23 21:28:51 +0530

    commiting after function based view

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,5 +2,6 @@ from restapi.views import *
 from django.urls import path
 
 urlpatterns = [
-    path("students/", students)
+    path("students/", students),
+    path("students/<int:id>/", getstudentbyid)
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -15,13 +15,32 @@ def students(request):
         students_serializer = Studentserializer(students, many = True)
         return Response(students_serializer.data,status = status.HTTP_200_OK)
     
-
     elif request.method == "POST":
         serializer = Studentserializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data,status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET","PUT","DELETE"])    
+def getstudentbyid(request, id):
+    try:
+        student = Students.objects.get(id = id)
+    except Students.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        student_get_serializer = Studentserializer(student)
+        return Response(student_get_serializer.data, status=status.HTTP_200_OK) 
+    elif request.method == "PUT":
+        update_serializer = Studentserializer(student,data = request.data)
+        if update_serializer.is_valid():
+            update_serializer.save()
+            return Response(update_serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+          
 
 '''    # Serializing in below way is not advisable
     students = Students.objects.all()
