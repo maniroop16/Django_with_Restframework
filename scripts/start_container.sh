67cc579c9eb41da09f27b4c0c9817d9cb8283382
maniroop16 <53170888+maniroop16@users.noreply.github.com>
2025-08-21 15:55:38 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull saimaniroopm1609/django-cicd-project:latest
 
 # Run the Docker image as a container
-docker run -t -p 8000:8000 saimaniroopm1609/django-cicd-project:latest
+docker run -d -p 8000:8000 saimaniroopm1609/django-cicd-project:latest
